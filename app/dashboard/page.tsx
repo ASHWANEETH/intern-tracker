9975a676767762aba69d819031d0abd67c67b0b9
ASHWANEETH <ashwaneeth@gmail.com>
2025-07-04 20:04:30 +0530

    refresh data on activr atab

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -108,7 +108,7 @@ export default function Dashboard() {
     };
 
     fetchUserAndJobs();
-  }, []);
+  }, [activeTab]);
 
   const handleLogout = async () => {
     await supabase.auth.signOut();
